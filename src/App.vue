e4ef8946dd43738fe009545f79b30d25e2616cca
Taras Vitovshchyk <vitovstreet@gmail.com>
2018-09-28 20:59:06 +0300

    first components

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<style lang="sass">
+    @import url('https://fonts.googleapis.com/icon?family=Material+Icons')
+    @import "./assets/style/main.sass"
+    @import "./assets/style/media.sass"
+</style>
+<template>
+  <div id="app" class="container">
+    <h1>User list!</h1>
+    <name-list></name-list>
+  </div>
+</template>
+
+<script>
+import NameList from './components/NameList'
+
+
+export default {
+  name: 'app',
+  components: {
+    NameList
+  },
+};
+</script>
+
---
+++ src/components/NameList.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="name-block">
+	    <ul class="header_list">
+	      <li></li>
+	      <li>Last</li>
+	      <li>First</li>
+	      <li>Username</li>
+	      <li>Phone</li>
+	      <li>Location</li>
+	    </ul>
+	    <ul class="user_list">
+	      <li class="people_cell" v-for="user in items.results" :key="user.seed">
+	      	<div class="main_info">
+	      		<div class="stnd_div">
+			      	<div class="img_user img_size">
+			      		<img :src="user.picture.large" alt=""/>
+			      	</div>
+	      		</div>
+		      	<div class="stnd_div last_name">{{user.name.last}}</div>
+		      	<div class="stnd_div first_name">{{user.name.first}}</div>
+		      	<div class="stnd_div first_name">{{user.login.username}}</div>
+		        <div class="stnd_div cell_user">{{user.cell}}</div>
+		        <div class="stnd_div location_user">{{user.location.state}}</div>
+		        <button class="btn_more">
+		        	<i class="material-icons">add</i>
+		        </button>
+	        </div>
+	        <div class="more_info">
+	        	<div class="main_name">{{user.name.first}} <span>{{user.gender}}</span></div>
+	        </div>
+	      </li>
+	    </ul>
+	</div>
+</template>
+
+
+
+<script>
+
+
+export default {
+	name: 'NameList',
+	data() {
+		return {
+			results: [],
+			items: []
+		}
+	},
+	mounted() {
+		fetch("https://randomuser.me/api/?results=6")
+		.then(response => response.json())
+		.then((data) => {
+			this.items = data;
+		})
+	},
+	methods: {
+
+	}
+}	
+
+</script>
